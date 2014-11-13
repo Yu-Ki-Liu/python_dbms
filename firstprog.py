571a959c36cecf10d5b69d2e9f3178b0805515cf
Yuki_Liu_mac <r03525057@ntu.edu.tw>
2014-11-13 10:52:14 +0800

    import open_file

--- firstprog.py
+++ firstprog.py
@@ -1,12 +1,14 @@
-
+import opne_file
 #open a file : ab  the file is in the append mode
-fo = open("user.txt","ab")
-print "Name of the file: ", fo.name
+opne_file.open_file()
 
-fo.write("dddYuki main\n");
+#fo = open("user.txt","ab")
+#print "Name of the file: ", fo.name
+
+#fo.write("dddYuki main\n");
 # Close opend file
-fo.close()
-print "Closed or not : ", fo.closed
-print "Opening mode : ", fo.mode
+#fo.close()
+#print "Closed or not : ", fo.closed
+#print "Opening mode : ", fo.mode
 #name = raw_input('Floor ?')
 #print int(name)
--- opne_file.py
+++ opne_file.py
@@ -1,7 +1,9 @@
 __author__ = 'chaucer'
-#open a file
-fo = open("user.txt","wb")
-print "Name of the file: ", fo.name
-print "Closed or not : ", fo.closed
-print "Opening mode : ", fo.mode
-print "Softspace flag : ", fo.softspace
+
+def open_file():
+    #open a file
+    fo = open("user.txt","wb")
+    print "Name of the file: ", fo.name
+    print "Closed or not : ", fo.closed
+    print "Opening mode : ", fo.mode
+    print "Softspace flag : ", fo.softspace
